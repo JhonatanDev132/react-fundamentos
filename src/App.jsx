eceea54850f9888e2114fd390de245501b424ff3
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2023-11-29 11:50:11 -0300

    ala

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import Cabecalho from "./Cabecalho";
+import Cabecalho from "./components/Cabecalho";
+import Conteudo from "./components/Conteudo";
+import Rodape from "./components/Rodape";
 
 /* Sempre os componentes tem que ser começar com maiúsculas */
 function App() {
@@ -6,47 +8,9 @@ function App() {
     <>
       <Cabecalho />
 
-      <main>
-        <h2>Teste</h2>
-        <section>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta iure
-            aliquid doloribus eveniet ipsam, debitis a, eligendi deserunt,
-            voluptatum qui explicabo officiis corrupti cupiditate voluptas.
-            Voluptates expedita ea quo dolorem? Nemo non voluptatem doloribus
-            quod, labore libero quo nesciunt sapiente iure illum tempore eum
-            eius nulla nihil possimus, perferendis aliquid magnam dignissimos
-            voluptate pariatur dolorem ex hic? Debitis, suscipit sapiente! Sit
-            porro rerum facere eos doloremque quia nemo facilis illo
-            repellendus! Nobis iure saepe deserunt quos, placeat beatae! Vitae
-            mollitia molestiae enim culpa quisquam dolores, sequi ab doloremque
-            blanditiis eum! Cum inventore magni suscipit id ipsa ad quae impedit
-            minima, tempore pariatur nihil! Neque ex quos est a ipsum porro at,
-            voluptatum nam ut fuga nihil atque, facere, vitae tenetur. Fugit
-            voluptate officia rem? In, vitae labore numquam earum officia amet
-            cum facere placeat magni porro esse iste, voluptatem hic. Aspernatur
-            facilis sed tempore, ullam voluptate modi sint quas iure.
-          </p>
+      <Conteudo />
 
-          <article>
-            <h3>Artigo 1</h3>
-            <p>Conteúdo artigo 1....</p>
-          </article>
-
-          <article>
-            <h3>Artigo 2</h3>
-            <p>Conteúdo artigo 2....</p>
-          </article>
-
-          <article>
-            <h3>Artigo 3</h3>
-            <p>Conteúdo artigo 3....</p>
-          </article>
-        </section>
-      </main>
-      <footer>
-        <h2>Rodapé da aplicação</h2>
-      </footer>
+      <Rodape />
     </>
   );
 }
---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,44 @@
+function Conteudo() {
+  return (
+    <main>
+      <h2>Teste</h2>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta iure
+          aliquid doloribus eveniet ipsam, debitis a, eligendi deserunt,
+          voluptatum qui explicabo officiis corrupti cupiditate voluptas.
+          Voluptates expedita ea quo dolorem? Nemo non voluptatem doloribus
+          quod, labore libero quo nesciunt sapiente iure illum tempore eum eius
+          nulla nihil possimus, perferendis aliquid magnam dignissimos voluptate
+          pariatur dolorem ex hic? Debitis, suscipit sapiente! Sit porro rerum
+          facere eos doloremque quia nemo facilis illo repellendus! Nobis iure
+          saepe deserunt quos, placeat beatae! Vitae mollitia molestiae enim
+          culpa quisquam dolores, sequi ab doloremque blanditiis eum! Cum
+          inventore magni suscipit id ipsa ad quae impedit minima, tempore
+          pariatur nihil! Neque ex quos est a ipsum porro at, voluptatum nam ut
+          fuga nihil atque, facere, vitae tenetur. Fugit voluptate officia rem?
+          In, vitae labore numquam earum officia amet cum facere placeat magni
+          porro esse iste, voluptatem hic. Aspernatur facilis sed tempore, ullam
+          voluptate modi sint quas iure.
+        </p>
+
+        <article>
+          <h3>Artigo 1</h3>
+          <p>Conteúdo artigo 1....</p>
+        </article>
+
+        <article>
+          <h3>Artigo 2</h3>
+          <p>Conteúdo artigo 2....</p>
+        </article>
+
+        <article>
+          <h3>Artigo 3</h3>
+          <p>Conteúdo artigo 3....</p>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default Conteudo;
