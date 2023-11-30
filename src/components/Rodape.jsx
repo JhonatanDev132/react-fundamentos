68f25b4cad2fbc68ada9e132e32f280b05335db1
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2023-11-30 09:41:25 -0300

    Estilos no rodapé

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,11 +1,13 @@
 function Rodape() {
+  const exemploEstilo = {
+    backgroundColor: "gray",
+    padding: "2%",
+    color: "white",
+    border: "solid 4px #F00",
+  };
+
   return (
-    <footer
-      style={{
-        backgroundColor: "gray",
-        padding: "2%",
-      }}
-    >
+    <footer style={exemploEstilo}>
       <h2>Rodapé da aplicação</h2>
     </footer>
   );
