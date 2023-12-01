ccbd208126ab9cc8da5ef433badb421eceb0e919
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2023-12-01 10:38:44 -0300

    Update Rodape.jsx

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,12 +1,23 @@
 import styled from "styled-components";
 
-const StyledRodape = styled.footer``;
+const StyledRodape = styled.footer`
+  background: rgb(2, 0, 36);
+  background: linear-gradient(90deg, #020024 0%, #302c9b 43%, #00d4ff 100%);
+  color: aliceblue;
+  height: 8rem;
+  position: relative;
+  top: 12.3rem;
+  text-align: center;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+`;
 
 function Rodape() {
   return (
-    <footer>
+    <StyledRodape>
       <h2>Rodapé da aplicação</h2>
-    </footer>
+    </StyledRodape>
   );
 }
 
