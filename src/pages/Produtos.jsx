1ba9a492534166b797bcb5a8cb2112262b2aeecf
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-01-15 11:28:33 -0300

    Update Produtos.jsx

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -11,8 +11,19 @@ function Produtos() {
 
     /* Gerenciando o efeito colateral
     do componente para o carregamento
-    dos dados da API. */
-    useEffect( () => {const carregarDados = async () => {
+    dos dados da API. 
+    
+    Fluxo geral de funcionamento do código abaixo: 
+
+    1) useEffect é carregado UMA VEZ a montagem da page (Produtos)
+    OBS.:o [] indica que o useEffect não tem dependências
+    adicionais e que será executado somente UMA VEZ.
+
+    2) Em seguida, ele executa a função carregarDados.
+    3) Ao término dela, atualiza o state (produtos)
+    */
+    useEffect( () => {
+        const carregarDados = async () => {
         try{
             const resposta = await fetch('https://fakestoreapi.com/products');
             const dados = await resposta.json();
