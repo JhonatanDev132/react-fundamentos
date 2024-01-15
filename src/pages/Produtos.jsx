0473183f976b00056b1c69393801a49a486bdcdb
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-01-15 11:07:11 -0300

    Update Produtos.jsx

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 function Produtos() {
+
+    const [produtos, setProdutos] = useState();
+
+    const carregarDados = async () => {
+        try{
+            const resposta = await fetch('https://fakestoreapi.com/products');
+            const dados = await resposta.json();
+            console.log(dados);
+        } catch (error) {
+            console.error("Houve um erro: "+error);
+        }
+    }
+
+    carregarDados();
+
     return <h2>Produtos</h2>;
 }
 
